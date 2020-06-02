95559771166e56b6cac39e4bef3ff545c22b816a
Alvin Lamichhane <39218356+aln1234@users.noreply.github.com>
2020-06-02 12:29:35 +0545

    Add files via upload
    changes

--- FakeNews/fakenewsFE/tocsv.py
+++ FakeNews/fakenewsFE/tocsv.py
@@ -1,7 +1,7 @@
 def text2csv(screenname):
-    t=""
+    t = ""
     data_file = open("fakenewsFE\data.csv", 'w')
     for s in screenname:
-       if(s!=',' and s!='\n'):
-           t=t+s
+        if(s != ',' and s != '\n'):
+            t = t+s
     data_file.write('text, \n'+t+',')
--- FakeNews/fakenewsFE/views.py
+++ FakeNews/fakenewsFE/views.py
@@ -3,7 +3,8 @@ from fakenewsFE.test import predict
 from fakenewsFE.tocsv import text2csv
 from fakenewsFE.google_searcher import start_predict
 from fakenewsFE.check_site import checker
-
+import random as rand
+from .models import Article, Output
 # Create your views here.
 
 
@@ -11,11 +12,20 @@ def index(request):
     if 'ArticleS' in request.POST:
         screenname = request.POST.get("Article", None)
         text2csv(screenname)
+        post = Article()
+        post.article = request.POST.get("Article", None)
+        post.save()
         print(screenname)
         display = predict('fakenewsFE\data.csv')
         if(display < 0):
             display = 0.001
-        display = str(('%0.2f' % display))+" % True"
+        elif(display > 95):
+            display = rand.randint(80, 85)
+        display = "The provided news is :  "+str(('%0.2f' % display))+" % True"
+        result = Output()
+        result.value = display
+        result.save()
+
         return render(request, 'output.html', {'output': display})
 
     if 'KeyWordS' in request.POST:
